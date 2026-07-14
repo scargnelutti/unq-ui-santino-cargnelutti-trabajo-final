04cd4d73063d6de36c87322eade23de1c38f8f69
Santino <scargnelutti@protonmail.com>
2026-07-14 10:36:19 -0300

    realizo el input

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,17 @@ function App() {
   const timerRef = useRef(null);
   const [isWrongWord, setIsWrongWord] = useState(false);
   const [points, setPoints] = useState(0);
+  const [usedWords, setUsedWords] = useState([]);
+  const [isWordRepeated, setIsWordRepeated] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (usedWords.includes(input)) {
+      console.log("INCLUYE?", input);
+      setIsWordRepeated(true);
+      return;
+    }
 
     // timerRef.current = setTimeout(() => {
     //   console.log("Esperando")
@@ -20,9 +28,12 @@ function App() {
       const { exists } = response.data
       if (exists) {
         console.log("Bien");
+        setUsedWords((prevState) => [...prevState, input]);
+        setCurrentWord(input);
         setInput(input.at(-1));
         setIsWrongWord(false);
         setPoints(points + 1);
+        setIsWordRepeated(false);
       } else {
         setIsWrongWord(true);
         console.log("Mal");
@@ -34,36 +45,54 @@ function App() {
     })
   }
 
-  const handleChange = (e) => {
-    setInput(e.target.value);
-  }
+  // const handleChange = (e) => {
+  //   if (currentWord === '') {
+  //     setInput(e.target.value);
+  //     return;
+  //   }
+  //
+  //   if (input.startsWith(currentWord.at(-1))) {
+  //     setInput(e.target.value);
+  //   } else {
+  //     setInput(currentWord.at(-1))
+  //   }
+  // }
 
+  const handleChange = (e) => {
+    const currentInput = e.target.value;
+    const lastLetter = currentWord.at(-1);
+
+    if (!currentWord || currentInput.startsWith(lastLetter)) {
+      setInput(currentInput);
+    } else {
+      setInput(lastLetter);
+    }
+  };
+
+  // const [timer, { startCountdown}] = useCountdown({countStart, intervalMs: 1000});
   return (
-    <>
-      <div className="container">
-        <div className="playing">
-          <div className="info">
-            {currentWord === '' ? (
-                <>
-                  <p>Escribí una palabra para comenzar</p>
-                </>
-              ) : (
-                  currentWord
-              )}
-            <p>{points}</p>
-          </div>
-          <div className="inputForm">
-            <form onSubmit={handleSubmit}>
-              <input id="textInput" type="text" value={input} onChange={handleChange} />
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-          <div className="feedback">
-            {isWrongWord && (<p>No es una palabra válida</p>)}
-          </div>
+    <div className="container">
+      <div className="playing">
+        <div className="info">
+          {currentWord === '' ? (
+              <p>Escribí una palabra para comenzar</p>
+            ) : (
+                <p>{currentWord}</p>
+            )}
+          <p>{points}</p>
+        </div>
+        <div className="inputForm">
+          <form onSubmit={handleSubmit}>
+            <input id="textInput" type="text" value={input} onChange={handleChange} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div className="feedback">
+          {isWrongWord && (<p>No es una palabra válida</p>)}
+          {isWordRepeated && (<p>La palabra ya fue usada</p>)}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
